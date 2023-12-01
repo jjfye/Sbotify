2c4b29be563e6f457aba176cdf2ad8744a7d7ab3
JjfyeDev <jye882@gmail.com>
2023-12-01 15:28:38 +0000

    Login/Signup buttons added

--- components/Button.tsx
+++ components/Button.tsx
@@ -23,6 +23,7 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
             py-3
             disabled:cursor-not-allowed
             disabled:opacity-50
+            text-black
             font-bold
             hover:opacity-75
             transition
